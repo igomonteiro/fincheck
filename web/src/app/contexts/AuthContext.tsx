570fc89e8a2b4943c0d09776629ac911170b4550
Igo B. Monteiro <igobmonteiro@gmail.com>
2023-08-07 23:18:58 -0300

    add authentication flow

--- web/src/app/contexts/AuthContext.tsx
+++ web/src/app/contexts/AuthContext.tsx
@@ -1,7 +1,14 @@
-import React, { createContext } from "react";
+import React, { createContext, useCallback, useEffect, useState } from "react";
+import { localStorageKeys } from "../config/localStorageKeys";
+import { useQuery } from "@tanstack/react-query";
+import { userService } from "../services/userService";
+import { toast } from "react-hot-toast";
+import { SplashScreen } from "../../ui/components/SplashScreen";
 
 interface AuthContextValue {
   signedIn: boolean;
+  signin(accessToken: string): void;
+  signout(): void;
 }
 
 export const AuthContext = createContext<AuthContextValue>(
@@ -9,13 +16,47 @@ export const AuthContext = createContext<AuthContextValue>(
 );
 
 export function AuthProvider({ children }: { children: React.ReactNode }) {
+  const [signedIn, setSignedIn] = useState<boolean>(() => {
+    const storedAccessToken = localStorage.getItem(
+      localStorageKeys.ACCESS_TOKEN,
+    );
+    return Boolean(storedAccessToken);
+  });
+
+  const { isError, isFetching, isSuccess, remove } = useQuery({
+    queryKey: ["users", "me"],
+    queryFn: () => userService.me(),
+    enabled: signedIn,
+  });
+
+  const signin = useCallback((accessToken: string) => {
+    localStorage.setItem(localStorageKeys.ACCESS_TOKEN, accessToken);
+    setSignedIn(true);
+  }, []);
+
+  const signout = useCallback(() => {
+    localStorage.removeItem(localStorageKeys.ACCESS_TOKEN);
+    remove();
+    setSignedIn(false);
+  }, [remove]);
+
+  useEffect(() => {
+    if (isError) {
+      toast.error("Sua sessão expirou!");
+      signout();
+    }
+  }, [isError, signout]);
+
   return (
     <AuthContext.Provider
       value={{
-        signedIn: true,
+        signedIn: isSuccess && signedIn,
+        signin,
+        signout,
       }}
     >
-      {children}
+      <SplashScreen isLoading={isFetching} />
+      {!isFetching && children}
     </AuthContext.Provider>
   );
 }
